d3d16f26ac3edcca5b4e3bddc6d8ec12b896589c
rjbaird <rjbaird09@gmail.com>
2022-03-27 15:23:15 -0500

    update CHANGELOG for 0.9.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,15 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.9.1] - 2022-03-27
+### Fixed
+- Updated welcomeMessage to better explain url routing
+
+## [0.9.0] - 2022-03-27
+### Added:
+- Versioning to url
+- More data is now pulled from both viz.com & viz.com/shonenjump for each manga
+- Data is now pulled from database instead of scraping each requests
 
 ## [0.5.1] - 2022-02-17
 ### Added:
--- src/services/health.services.ts
+++ src/services/health.services.ts
@@ -1,12 +1,11 @@
 export const getWelcomMessage = () => {
   return {
-    title: "Welcome to the unofficial shonen-jump-api",
-    description:
-      "A REST API showing data about English translations of Weekly Shonen Jump available on Viz.com",
-    repo: "https://github.com/Rjbaird/shonen-jump-api",
-    help: "https://github.com/Rjbaird/shonen-jump-api/issues",
-    rapidAPI: "https://rapidapi.com/Rjbaird/api/unofficial-shonen-jump",
-    version: "v1",
-    endpoints: ["/manga", "/manga/:mangaID", "/schedule"],
+    title: 'Welcome to the unofficial shonen-jump-api',
+    description: 'A REST API showing data about English translations of Weekly Shonen Jump available on Viz.com',
+    repo: 'https://github.com/Rjbaird/shonen-jump-api',
+    help: 'https://github.com/Rjbaird/shonen-jump-api/issues',
+    rapidAPI: 'https://rapidapi.com/Rjbaird/api/unofficial-shonen-jump',
+    version: 'v0.9.0',
+    endpoints: ['/v1/manga', '/v1/manga/:mangaID', '/v1/schedule'],
   };
 };
